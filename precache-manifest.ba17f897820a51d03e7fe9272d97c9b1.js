9fe23b05e12742f7279de8b449657f940c9b2d8c
mra-abrahamson <mra.abrahamson@gmail.com>
2019-01-30 14:55:13 -0800

    Updates

---
+++ precache-manifest.ba17f897820a51d03e7fe9272d97c9b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "704f54f75d7a1d17898c",
+    "url": "/Clicky-Game/static/js/main.704f54f7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Clicky-Game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "704f54f75d7a1d17898c",
+    "url": "/Clicky-Game/static/css/main.1d7ef8d7.chunk.css"
+  },
+  {
+    "revision": "ba3323366602eaca9237ba70888eb330",
+    "url": "/Clicky-Game/index.html"
+  }
+];
